09bf45913b6df29bd20c17e5c19b34c07c40a562
themultiplexer <joshua.rutschmann@gmx.de>
2022-03-30 13:15:33 +0200

    Projektdateien hinzufügen.

---
+++ MicFx/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace MicFx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 403);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(795, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(871, 403);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1984, 593);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ MicFx/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MicFx
+{
+
+    public partial class Form1 : Form
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 4, CharSet = CharSet.Auto)]
+        public struct WAVEINCAPS
+
+        {
+            public ushort wMid;
+            public ushort wPid;
+            public uint vDriverVersion;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 1024)]
+            public string szPname;
+            public uint dwFormats;
+            public ushort wChannels;
+            public ushort wReserved1;
+
+        }
+
+        [DllImport("winmm.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern uint waveInGetDevCaps(uint uDeviceID, ref WAVEINCAPS pwic, uint cbwic);
+
+        [DllImport("winmm.dll", EntryPoint = "waveInGetNumDevs", SetLastError = true)]
+        public static extern uint waveInGetNumDevs();
+
+        [StructLayout(LayoutKind.Sequential, Pack = 4, CharSet = CharSet.Auto)]
+        public struct WAVEOUTCAPS
+        {
+            public short wMid;
+            public short wPid;
+            public int vDriverVersion;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string szPname;
+            public uint dwFormats;
+            public short wChannels;
+            public short wReserved;
+            public uint dwSupport;
+        }
+
+        [DllImport("winmm.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern uint waveOutGetDevCaps(uint hwo, ref WAVEOUTCAPS pwoc, uint cbwoc);
+
+        [DllImport("winmm.dll", SetLastError = true)]
+        static extern uint waveOutGetNumDevs();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (string s in GetRecordingDeviceList()) { 
+                richTextBox1.Text += s + "\n";
+            }
+
+            foreach (string s in GetPlaybackDeviceList())
+            {
+                richTextBox2.Text += s + "\n";
+            }
+        }
+
+        public string[] GetRecordingDeviceList()
+        {
+            uint total = waveInGetNumDevs();
+            string[] list = new string[total];
+            WAVEINCAPS pwoc = new WAVEINCAPS();
+            for (uint i = 0; i < total; i++)
+            {
+                waveInGetDevCaps(i, ref pwoc, (uint)Marshal.SizeOf(typeof(WAVEINCAPS)));
+                list[i] = pwoc.szPname;
+            }
+            return list;
+        }
+
+        public string[] GetPlaybackDeviceList()
+        {
+            uint total = waveOutGetNumDevs();
+            string[] list = new string[total];
+            WAVEOUTCAPS pwoc = new WAVEOUTCAPS();
+            for (uint i = 0; i < total; i++)
+            {
+                waveOutGetDevCaps(i, ref pwoc, (uint)Marshal.SizeOf(typeof(WAVEOUTCAPS)));
+                list[i] = pwoc.szPname;
+            }
+            return list;
+        }
+    }
+}
